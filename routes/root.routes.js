e268cf49bbfd3f1ddf0bb9bf1f94229fbd1934ab
csdivyansh <idivyanshv@gmail.com>
2025-06-28 15:57:42 +0530

    rename root to use html

--- routes/root.routes.js
+++ routes/root.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("This is the root route");
+  res.send("<h1>Welcome to the Root Route</h1>");
 });
 
 export default router;
