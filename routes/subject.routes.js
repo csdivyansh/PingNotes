4002f67b36f30d6c5f3c69afb1ad6b3b02e9bed8
csdivyansh <idivyanshv@gmail.com>
2025-07-04 17:18:19 +0530

    added vercel.json

--- routes/subject.routes.js
+++ routes/subject.routes.js
@@ -3,8 +3,10 @@ import {
     getAllSubjects,
     createNewSubject
 } from "../controllers/subject/subject.controller.js";
+import { requireAuth } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
+router.use(requireAuth(["admin", "teacher", "user"]));
 
 router.get("/", getAllSubjects);
 router.post("/", createNewSubject);
