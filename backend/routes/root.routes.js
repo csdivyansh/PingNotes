21b13c2d89e0c7f73b318adbb91da8aae300be64
csdivyansh <idivyanshv@gmail.com>
2025-07-23 22:30:14 +0530

    added cron job

--- backend/routes/root.routes.js
+++ backend/routes/root.routes.js
@@ -2,7 +2,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("<h1>Welcome to the Root Route</h1>");
+  res.send("<h1>Pingnotes is live!</h1>");
 });
 
 export default router;
